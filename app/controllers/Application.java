f17371860bde2ca5f7d771ec43a89b1d54119156
Ubuntu 12.04 32bits <uservm@ubuntu.(none)>
2016-01-14 03:11:28 -0800

    Gmail: bypass du formulaire Get Token - part 3 redirection vers la liste de contacts (26)

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -149,8 +149,10 @@ public class Application extends Controller {
     	
     	// Make GET request at Google to get contacts
 		//WS.HttpResponse contactResponse = WS.url( "https://www.google.com/m8/feeds/contacts/default/full?access_token=" + accessToken ).get() ;
+		WS.HttpResponse contactResponse = WS.url( "https://www.google.com/m8/feeds/contacts/default/full").setParameter("access_token", accessToken).get() ;
 		
-		WS.HttpResponse contactResponse = WS.url( "https://www.playframework.com/documentation/1.3.x/libs#ParsingXMLusingXPath" ).get() ;
+		
+		//WS.HttpResponse contactResponse = WS.url( "https://www.playframework.com/documentation/1.3.x/libs#ParsingXMLusingXPath" ).get() ;
 		
 		org.w3c.dom.Document xmlDoc = contactResponse.getXml() ;						// Get Xml document from response at GET request
 		String nb = Integer.toString(contactResponse.getStatus()) ;
